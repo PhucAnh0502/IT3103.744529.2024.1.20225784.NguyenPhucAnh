346b5fa4779c12d253824b14efc97966ec15c99a
PhucAnh0502 <phucanh05022004@gmail.com>
2024-09-27 22:25:52 +0700

    update files

--- Lab01/HelloNameDialog.java
+++ Lab01/HelloNameDialog.java
@@ -2,8 +2,8 @@ import javax.swing.JOptionPane;
 public class HelloNameDialog{
 	public static void main(String[] args){
 		String result;
-		result = JOptionPane.showInputDialog("Please enter your name:");
-		JOptionPane.showMessageDialog(null, "Hi "+result+"!");
+		result = JOptionPane.showInputDialog("Nguyen Phuc Anh - 20225784 Please enter your name:");
+		JOptionPane.showMessageDialog(null, "Nguyen Phuc Anh - 20225784 Hi "+result+"!");
 		System.exit(0);
 	}
 }
--- Lab01/HelloWorld.java
+++ Lab01/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String args[]) {
+		System.out.println("Nguyen Phuc Anh - 20225784");
 		System.out.println("Xin chao \n cac ban");
 		System.out.println("Hello \t World!");
 	}
