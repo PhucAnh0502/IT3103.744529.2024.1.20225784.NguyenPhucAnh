8ba2fe24a9608c9102be68421adbd453f44dfc97
PhucAnh0502 <phucanh05022004@gmail.com>
2024-09-29 22:01:20 +0700

    Exercise 6.2

---
+++ Lab01/InputFromKeyboard.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class InputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Nguyen Phuc Anh - 20225784 : What's your name?");
+        String strName = keyboard.nextLine();
+        System.out.println("Nguyen Phuc Anh - 20225784 : How old are you?");
+        int iAge = keyboard.nextInt();
+        System.out.println("nguyen Phuc Anh - 20225784 : How tall are you (m)?");
+        double dHeight = keyboard.nextDouble();
+        System.out.println("Nguyen Phuc Anh - 20225784 : Mrs/Mr. " + strName + ", " + iAge + " years old. " +
+                            "Your height is " + dHeight + ".");
+    }
+}
