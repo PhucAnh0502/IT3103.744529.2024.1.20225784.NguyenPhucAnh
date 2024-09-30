c10941027cfb8e233990df5f1f1e9cf6c30e91fa
PhucAnh0502 <phucanh05022004@gmail.com>
2024-09-30 20:19:12 +0700

    update file

--- Lab01/Calculate.java
+++ Lab01/Calculate.java
@@ -3,10 +3,12 @@ public class Calculate{
     public static void main(String[] args){
         String strNum1, strNum2;
         strNum1 = JOptionPane.showInputDialog(null,
-                "Nguyen Phuc Anh - 20225784 - Please input the first number: ", "Nguyen Phuc Anh - 20225784 - Input the first number",
+                "Nguyen Phuc Anh - 20225784 - Please input the first number: ", 
+                "Nguyen Phuc Anh - 20225784 - Input the first number",
                 JOptionPane.INFORMATION_MESSAGE);
         strNum2 = JOptionPane.showInputDialog(null,
-                "Nguyen Phuc Anh - 20225784 - Please input the second number: ", "Nguyen Phuc Anh - 20225784 - Input the second number",
+                "Nguyen Phuc Anh - 20225784 - Please input the second number: ", 
+                "Nguyen Phuc Anh - 20225784 - Input the second number",
                 JOptionPane.INFORMATION_MESSAGE);
         double num1 = Double.parseDouble(strNum1);
         double num2 = Double.parseDouble(strNum2);
